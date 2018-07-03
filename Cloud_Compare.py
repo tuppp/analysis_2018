43efec62d233d3de3734903e4ac22e213e09debd
KyraKerz <kyra@mailbox.tu-berlin.de>
2018-07-03 11:22:35 +0200

    added min_temp compare

--- Cloud_Compare.py
+++ Cloud_Compare.py
@@ -23,35 +23,73 @@ wetter_de, se5 = fle.getConnectionWetterde()
 wetter_dienst, se6 = fle.getConnectionWetterdienstde()
 
 
+dwd_porperty_index = 2
+app_property_index = 3
 
-coverage_amount_index = 2
-cloud_index = 3
+"""
+Queries:
+1. Min_Temp
+2. Max_Temp
+3. Clouds
+"""
+
+
+
+def min_temp_select(weatherapp):
+
+    if weatherapp == 'accuweathercom':
+        accuweather_compare_query = se2.execute('select dwd.postcode, dwd.measure_date, dwd.min_temp, app.min_temp from dwd, accuweathercom as app where dwd.min_temp <> NULL and app.min_temp <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
+        min_temp_compare = fle.getResult(se2, accuweather_compare_query)
+    elif weatherapp == 'openweathermaporg':
+        openweather_compare_query = se3.execute(
+        'select dwd.postcode, dwd.measure_date, dwd.min_temp, app.min_temp from dwd, openweathermaporg as app where dwd.min_temp <> NULL and app.min_temp <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
+        min_temp_compare = fle.getResult(se3, openweather_compare_query)
+    elif weatherapp == 'wettercom':
+        wettercom_compare_query = se4.execute(
+        'select dwd.postcode, dwd.measure_date, dwd.min_temp, app.min_temp from dwd, wettercom as app where dwd.min_temp <> NULL and app.min_temp <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
+        min_temp_compare = fle.getResult(se4, wettercom_compare_query)
+    elif weatherapp == 'wetterde':
+        wetterde_compare_query = se5.execute(
+        'select dwd.postcode, dwd.measure_date, cdwd.min_temp, app.min_temp from dwd, wetterde as app where dwd.min_temp <> NULL and app.min_temp <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
+        min_temp_compare = fle.getResult(se5, wetterde_compare_query)
+    else:
+        wetterdienstde_compare_query = se6.execute(
+        'select dwd.postcode, dwd.measure_date, dwd.min_temp, app.min_temp from dwd, wetterdienstde as app where dwd.min_temp <> NULL and app.min_temp <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
+        min_temp_compare = fle.getResult(se6, wetterdienstde_compare_query)
+
+    diff(min_temp_compare)
+    mean_square_error(min_temp_compare)
+    return min_temp_compare
+
+
+
+
 
 def cloud_select(weatherapp):
 
     if weatherapp == 'accuweathercom':
-        accuweather_compare_query = se2.execute('select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, openweathermaporg where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = openweathermaporg.measure_date and dwd.postcode <> openweathermaporg.postcode');
+        accuweather_compare_query = se2.execute('select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, accuweathercom as app where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
         cloud_compare = fle.getResult(se2, accuweather_compare_query)
     elif weatherapp == 'openweathermaporg':
         openweather_compare_query = se3.execute(
-        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, openweathermaporg where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = openweathermaporg.measure_date and dwd.postcode <> openweathermaporg.postcode');
+        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, openweathermaporg as app where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
         cloud_compare = fle.getResult(se3, openweather_compare_query)
     elif weatherapp == 'wettercom':
         wettercom_compare_query = se4.execute(
-        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, openweathermaporg where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = openweathermaporg.measure_date and dwd.postcode <> openweathermaporg.postcode');
+        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, wettercom as app where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
         cloud_compare = fle.getResult(se4, wettercom_compare_query)
     elif weatherapp == 'wetterde':
         wetterde_compare_query = se5.execute(
-        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, openweathermaporg where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = openweathermaporg.measure_date and dwd.postcode <> openweathermaporg.postcode');
+        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, wetterde as app where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
         cloud_compare = fle.getResult(se5, wetterde_compare_query)
     else:
         wetterdienstde_compare_query = se6.execute(
-        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, openweathermaporg where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = openweathermaporg.measure_date and dwd.postcode <> openweathermaporg.postcode');
+        'select dwd.postcode, dwd.measure_date, coverage_amount, clouds from dwd, wetterdienstde as app where clouds <> NULL and coverage_amount <> NULL and dwd.measure_date = app.measure_date and dwd.postcode = app.postcode');
         cloud_compare = fle.getResult(se6, wetterdienstde_compare_query)
 
-    cloud_diff(cloud_compare)
-    cloud_spearman(cloud_compare)
-    clouds_mean_square_error(cloud_compare)
+    diff(cloud_compare)
+    spearman(cloud_compare)
+    mean_square_error(cloud_compare)
     return cloud_compare
 
 
@@ -61,7 +99,7 @@ def cloud_select(weatherapp):
 
 def cloud_convert_dwd(dwd_waetherapp_query_result):
     for object in dwd_waetherapp_query_result:
-        object[coverage_amount_index] = np.round(coverage_amount_index)
+        object[dwd_porperty_index] = np.round(dwd_porperty_index)
 
 
 def cloud_convert_weather_app(dwd_waetherapp_query_result):
@@ -70,49 +108,49 @@ def cloud_convert_weather_app(dwd_waetherapp_query_result):
     intervall_ends = 100/9/2
 
     for weather_cloud in dwd_waetherapp_query_result:
-        if weather_cloud[cloud_index] == 'NULL':
+        if weather_cloud[app_property_index] == 'NULL':
             continue
-        weather_cloud[cloud_index] = weather_cloud[16]/9
-        if weather_cloud[cloud_index] <= intervall_ends:
-            weather_cloud[cloud_index] = 0
+        weather_cloud[app_property_index] = weather_cloud[16]/9
+        if weather_cloud[app_property_index] <= intervall_ends:
+            weather_cloud[app_property_index] = 0
         elif intervall_ends  < weather_cloud[16] <= intervall_ends + intervall_length:
-            weather_cloud[cloud_index] = 1
+            weather_cloud[app_property_index] = 1
         elif intervall_ends + intervall_length < weather_cloud[16] <= intervall_ends + intervall_length*2:
-            weather_cloud[cloud_index] = 2
+            weather_cloud[app_property_index] = 2
         elif intervall_ends + intervall_length*2 < weather_cloud[16] <= intervall_ends + intervall_length*3:
-            weather_cloud[cloud_index] = 3
+            weather_cloud[app_property_index] = 3
         elif intervall_ends + intervall_length*3 < weather_cloud[16] <= intervall_ends + intervall_length * 4:
-            weather_cloud[cloud_index] = 4
+            weather_cloud[app_property_index] = 4
         elif intervall_ends + intervall_length * 4 < weather_cloud[16] <= intervall_ends + intervall_length * 5:
-            weather_cloud[cloud_index] = 5
+            weather_cloud[app_property_index] = 5
         elif intervall_ends + intervall_length * 5 < weather_cloud[16] <= intervall_ends + intervall_length * 6:
-            weather_cloud[cloud_index] = 6
+            weather_cloud[app_property_index] = 6
         elif intervall_ends + intervall_length * 6 < weather_cloud[16] <= intervall_ends + intervall_length * 7:
-            weather_cloud[cloud_index] = 7
+            weather_cloud[app_property_index] = 7
         elif intervall_ends + intervall_length * 7 < weather_cloud[16] <= intervall_ends*2 + intervall_length * 8:
-            weather_cloud[cloud_index] = 8
+            weather_cloud[app_property_index] = 8
 
 
-def cloud_diff(dwd_waetherapp_query_result):
+def diff(dwd_waetherapp_query_result):
     for object in dwd_waetherapp_query_result:
-        cloud_diff = object[coverage_amount_index]- object[cloud_index]
+        cloud_diff = object[dwd_porperty_index]- object[app_property_index]
         dwd_waetherapp_query_result.append(cloud_diff)
 
 
-def clouds_mean_square_error(dwd_waetherapp_query_result):
+def mean_square_error(dwd_waetherapp_query_result):
     clouds_rmse = 0
     for object in dwd_waetherapp_query_result:
-        coverage_amount = object[coverage_amount_index]
-        clouds = object[cloud_index]
+        coverage_amount = object[dwd_porperty_index]
+        clouds = object[app_property_index]
         clouds_rmse = clouds_rmse+(((coverage_amount-clouds)**2))
     clouds_rmse = np.sqrt(clouds_rmse / len(dwd_waetherapp_query_result))
     dwd_waetherapp_query_result.append(clouds_rmse)
 
 
 
-def cloud_spearman(dwd_waetherapp_query_result):
-    cloud_list = dwd_waetherapp_query_result[:, cloud_index]
-    coverage_amount_list  =dwd_waetherapp_query_result[:, coverage_amount_index]
+def spearman(dwd_waetherapp_query_result):
+    cloud_list = dwd_waetherapp_query_result[:, app_property_index]
+    coverage_amount_list  =dwd_waetherapp_query_result[:, dwd_porperty_index]
     korr, pval = sc.stats.spearmanr(cloud_list, coverage_amount_list)
     dwd_waetherapp_query_result.append(korr)
     dwd_waetherapp_query_result.append(pval)
